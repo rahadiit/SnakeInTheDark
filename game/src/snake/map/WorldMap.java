77f4a20d50374addf45be1a7afeba76a61338f8d
MrStrings <phstring95@hotmail.com>
2015-05-10 01:40:24 -0300

    Light system demo just arrived for testing

--- game/src/snake/map/WorldMap.java
+++ game/src/snake/map/WorldMap.java
@@ -1,6 +1,7 @@
 package snake.map;
 
 import snake.engine.GameWorld;
+import snake.engine.MapEntity;
 import snake.engine.creators.WorldSettings;
 import snake.engine.visuals.Lights;
 import box2dLight.Light;
@@ -22,17 +23,25 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class WorldMap extends GameWorld {
 	
 	// The code below is simply a prototype for testing purposes 
-	private Texture texture;
 	private Sprite sprite;
+	private Sprite entity; //must be changed to Map Entities 
 	Light light;
-	private float i = .2f;
+	private float velocity = .2f;
+	private int x = 1;
+	private boolean y = false, triggered = false;
 	
 	public WorldMap (/* Add other parameters of choice*/) {
 		
-		texture = new Texture(Gdx.files.internal("foggy_forest_by_BrokenLens.jpeg"));
+		Texture texture = new Texture(Gdx.files.internal("foggy_forest_by_BrokenLens.jpeg"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		sprite = new Sprite(texture);
 		sprite.setSize(WorldSettings.getWorldWidth(), WorldSettings.getWorldHeight());
+		
+		Texture texture2 = new Texture(Gdx.files.internal("Scary_Silhouette.png"));
+		entity = new Sprite(texture2);
+		entity.setSize(4, 12);
+		entity.setAlpha(1f);
+		entity.setPosition(60, 15);
 		
 	}
 	
@@ -40,15 +49,40 @@ public class WorldMap extends GameWorld {
 	
 	@Override
 	public void act(float delta) {
-		if (light.getX() >= 100 || light.getX() <= 0)
-			i *=-1;
-		light.setPosition(light.getX() + i, light.getY());
+		if (light.getX() >= 100) {
+			velocity *=-1;
+			y = false;
+		} else if (light.getX() <= 0) {
+			x++;
+			if (x % 3 == 0) {
+				y = true;
+				entity.setPosition(entity.getX() - entity.getWidth()*1/2, entity.getY() - entity.getHeight()*3 /4);
+				entity.setSize(entity.getWidth()* 2f, entity.getHeight() * 2f);
+			}
+			velocity *= -1;
+		}
+		light.setPosition(light.getX() + velocity, light.getY());
+		
+		
+		if (x == 9 && light.getX() >= 90) {
+			triggered = true;
+			WorldSettings.setAmbientColor(new Color(1f, 0, 0, 1f));
+		}
+		
+		if (triggered && light.getX() > 91) {
+			Gdx.app.exit();
+		}
+		
+		
 	}
 	
 	@Override
 	public void draw (Batch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
 		sprite.draw(batch);
+		
+		if (y)
+			entity.draw(batch);
 	}
 	
 	@Override
@@ -58,7 +92,7 @@ public class WorldMap extends GameWorld {
 	
 	@Override
 	public void dispose() {
-		texture.dispose();
+		//sprite.getTexture().dispose();
 		light.dispose();
 	}
 
