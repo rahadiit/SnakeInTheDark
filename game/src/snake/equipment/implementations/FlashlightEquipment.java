be77ee0cf29906c5caa46c53367315ae3a9c69e7
Beatriz Sechin Zazulla <bszazulla@gmail.com>
2015-06-04 21:30:56 -0300

    Flashlight arrumada de acordo com o motor gráfico

--- game/src/snake/equipment/implementations/FlashlightEquipment.java
+++ game/src/snake/equipment/implementations/FlashlightEquipment.java
@@ -1,11 +1,15 @@
 package snake.equipment.implementations;
 
 import box2dLight.ConeLight;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
 import snake.map.IMapAccess;
 import snake.visuals.Lights;
 
@@ -22,6 +26,7 @@ public class FlashlightEquipment extends AbstractEquipment
 	Texture texture = new Texture(Gdx.files.internal("PixelFlashlight.png"));
 	Sprite sprite = new Sprite(texture); 
 	ConeLight light;
+	Vector2 vec;
 	int cont;
 
 	// Construtor com parametros de posi��o de in�cio
@@ -31,6 +36,7 @@ public class FlashlightEquipment extends AbstractEquipment
 		this.description = "The Flashlight lights in front of you, but use with caution, it doesnt last long";
 		
 		this.setBounds(x, y, 5, 5); // ver o setPosition
+		vec = new Vector2();
 	}
 	
 	// Construtor sem par�metros
@@ -58,11 +64,17 @@ public class FlashlightEquipment extends AbstractEquipment
 	// Atualiza��o gr�fica no jogo
 	public void act (float delta) 
 	{
-		// esses getters s�o relativos � imagem da lanterna, para que a luz fique sempre junto dela
-		light.setPosition(getOriginX() + getX() + 3, getOriginY() + getY() + 3);
-		light.setDirection(sprite.getRotation() + 90); 
-		
-		// luz deve acompanhar o player
+		vec.set(0, 0);
+		this.localToStageCoordinates(vec);
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SHIFT_LEFT))
+		{
+			System.out.println(getX() + ":" + getY());
+			light.setActive(!light.isActive());
+		}
+
+		light.setPosition(vec);
+		light.setDirection(this.getParent().getRotation() + 90);
 	}
 
 	// THIS EQUIPMENT HAS LIGHTS
@@ -76,13 +88,17 @@ public class FlashlightEquipment extends AbstractEquipment
 	public void createLights()
 	{ // Criacao de luzes tem que ser algo separado (senao da pau) -- tudo aqui
 		light = new ConeLight(Lights.getRayhandler(), 5000, new Color(1f, 1f, .5f, 1f), 40, 45, 45, 45, 30);
+		light.setActive(false);
 	} // Se quiser destruir a luz, pode ser em qualquer lugar
 
 	@Override
 	public void disposeLights()
 	{
-		light.remove(); // IF you don't remove stuff gets crazy
-		light.dispose();
+		if (light != null)
+		{
+			light.remove();
+			light.dispose();
+		}
 	}
 	
 	@Override
