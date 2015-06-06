a383594b4aadd93a805bd278c2b0f5d583324127
Gabriel Gimenez <gabrieljgimenez@gmail.com>
2015-06-05 22:51:13 -0300

    .

--- game/src/snake/equipment/implementations/GunEquipment.java
+++ game/src/snake/equipment/implementations/GunEquipment.java
@@ -13,7 +13,7 @@ import snake.map.IMapAccess;
 public class GunEquipment extends AbstractEquipment
 {
 	boolean onMap;
-	Texture texture = new Texture(Gdx.files.internal("ammo.png"));
+	Texture texture = new Texture(Gdx.files.internal("PixelFlashlight.png"));
 	Sprite sprite = new Sprite(texture); 
 	public GunEquipment()
 	{
--- game/src/snake/equipment/implementations/TrapEquipment.java
+++ game/src/snake/equipment/implementations/TrapEquipment.java
@@ -15,7 +15,7 @@ import snake.map.IMapAccess;
 
 public class TrapEquipment extends AbstractEquipment
 {
-	Texture texture = new Texture(Gdx.files.internal("trap.png"));
+	Texture texture = new Texture(Gdx.files.internal("PixelFlashlight.png"));
 	Sprite sprite = new Sprite(texture); 
 	boolean onMap;
 
