d9667f3be4e064a56140d2f4a7a46c4d28090735
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2015-06-05 22:57:56 -0300

    Inicializar equipamentos com informações do mapa

--- game/src/snake/map/MapManager.java
+++ game/src/snake/map/MapManager.java
@@ -6,13 +6,15 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import snake.engine.dataManagment.Loader;
+import snake.equipment.EquipmentCreator;
+import snake.equipment.IEquipment;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class MapManager implements IMapAccess {
 
@@ -26,16 +28,20 @@ public class MapManager implements IMapAccess {
     private TiledMap map;
 
     private final List<IMapEntity> entities = new LinkedList<>();
-    private final List<IMapEntity> immutableWrapper = Collections.unmodifiableList(entities);
+    private final List<IMapEntity> entitiesWrapper = Collections.unmodifiableList(entities);
+
+    private final List<String> availableEquipments = new ArrayList<>();
 
     private int mapWidth;
     private int mapHeight;
     private int tileWidth;
     private int tileHeight;
 
+    private Random random = new Random();
+
     @Override
     public List<IMapEntity> getEntities() {
-        return immutableWrapper;
+        return entitiesWrapper;
     }
 
     @Override
@@ -66,16 +72,53 @@ public class MapManager implements IMapAccess {
         assetManager.finishLoadingAsset(name);
         map = assetManager.get(name);
 
+        entities.clear();
+        availableEquipments.clear();
+
         MapProperties properties = map.getProperties();
-        mapWidth = properties.get("width", Integer.TYPE);
-        mapHeight = properties.get("height", Integer.TYPE);
-        tileWidth = properties.get("tilewidth", Integer.TYPE);
-        tileHeight = properties.get("tileheight", Integer.TYPE);
+        mapWidth = properties.get("width", Integer.class);
+        mapHeight = properties.get("height", Integer.class);
+        tileWidth = properties.get("tilewidth", Integer.class);
+        tileHeight = properties.get("tileheight", Integer.class);
+
+        String equips = properties.get("equipList", "", String.class);
+        Collections.addAll(availableEquipments, equips.split(","));
+        int equipQuantity = Integer.parseInt(properties.get("equipQuantity", "0", String.class));
+        spawnEquipments(equipQuantity);
     }
 
     public MapRenderer createRenderer() {
         int tileSize = Math.min(tileWidth, tileHeight);
         return new OrthogonalTiledMapRenderer(map, 1f / tileSize);
+    }
+
+    private void spawnEquipments(int equipQuantity) {
+        TiledMapTileLayer baseLayer = (TiledMapTileLayer) map.getLayers().get("base");
+
+        for (int i = 0; i < equipQuantity; i++) {
+            String cellType;
+            Cell cell;
+            int w, h;
+            boolean alreadySpawned;
+
+            do {
+                w = random.nextInt(mapWidth);
+                h = random.nextInt(mapHeight);
+
+                cell = baseLayer.getCell(w, h);
+                MapProperties properties = cell.getTile().getProperties();
+
+                cellType = properties.get("type", "", String.class);
+                alreadySpawned = properties.get("spawned", false, Boolean.class);
+            } while (!cellType.equals("floor") && !alreadySpawned);
+
+            cell.getTile().getProperties().put("spawned", true);
+
+            int index = random.nextInt(availableEquipments.size());
+            IEquipment equipment = EquipmentCreator.createFactory(availableEquipments.get(index)).create(0, 0, true);
+
+            addEntity(equipment);
+        }
     }
 
     public int getMapWidth() {
