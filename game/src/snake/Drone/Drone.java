54a32f1e26ea3dddb926ff0206316fa5144ab108
Agustina Diamint <Agustina.diamint@gmail.com>
2015-06-07 23:05:37 -0300

    Começo de drone

---
+++ game/src/snake/Drone/Drone.java
@@ -0,0 +1,57 @@
+package snake.Drone;
+
+import snake.engine.dataManagment.Loader;
+import snake.engine.models.GameWorld;
+import snake.visuals.enhanced.LightMapEntity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**                              Developed By:
+ *                                  NoDark
+ *                               sessaGlasses
+ *                               
+ * 
+ * 
+ * @author Gus
+ */
+
+public class Drone extends LightMapEntity {
+	private Sprite sprite;
+	private String Direction;
+	private String textName = "character/Drone";
+	
+	public Drone (GameWorld world, int x, int y, String Direcao){
+		super(world);
+		world.addActor(this);
+		this.setSize(2.5f,3);
+		//this.setOrigin(), valor dado por quem chama , mapa?
+		
+		
+		Loader.load(textName,Texture.class);
+		sprite = new Sprite(texture);
+		
+		sprite.setAlpha();
+	}
+	
+	public void act(){
+		if(Direction.equalsIgnoreCase("Esquerda"))
+			moveby(-2.5f,0);
+			
+		else if(Direction.equalsIgnoreCase("Direita"))
+			moveby(2.5f,0);
+		
+		else if(Direction.equalsIgnoreCase("Cima"))
+			moveby(0,2.5f);
+		
+		else if(Direction.equalsIgnoreCase("Baixo"))
+			moveby(0,2.5f);
+			
+		}
+		
+	}
+	
+}
