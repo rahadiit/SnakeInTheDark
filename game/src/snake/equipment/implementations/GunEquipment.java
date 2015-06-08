55b94567b418959c3baa8096e98e153d849669fb
Gabriel Gimenez <gabrieljgimenez@gmail.com>
2015-06-08 18:51:59 -0300

    Atualizacao na implementacao dos equipamentos

--- game/src/snake/equipment/implementations/GunEquipment.java
+++ game/src/snake/equipment/implementations/GunEquipment.java
@@ -14,18 +14,17 @@ import snake.map.IMapEntity;
  * @author bszazulla
  */
 
-public class GunEquipment extends AbstractEquipment
-{
+public class GunEquipment extends AbstractEquipment {
 	boolean onMap;
 	Texture texture;
-	Sprite sprite; 
-	public GunEquipment(float x, float y, boolean onMap)
-	{
+	Sprite sprite;
+
+	public GunEquipment(float x, float y, boolean onMap) {
 		this.setBounds(x, y, 4, 4);
-		this.onMap = onMap; 
+		this.onMap = onMap;
 		this.name = "Gun";
 		this.description = "When a droid gets close to you, if you have ammo, it's destroyed";
-		
+
 		Loader.load("equipments/BulletPixel.png", Texture.class);
 		Loader.finishLoadingAsset("equipments/BulletPixel.png");
 		texture = Loader.get("equipments/BulletPixel.png");
@@ -35,29 +34,29 @@ public class GunEquipment extends AbstractEquipment
 
 	public int ammo;
 
-	public void activateOnMap(IMapAccess map) 
-	{
+	public void activateOnMap(IMapAccess map) {
 		IMapEntity entity = null;
-		if (ammo > 0)
-			map.removeEntity(entity);
+		if (ammo > 0) {
+			// map.removeEntity(map.getEntity((int)getX(), (int)getY());
+			ammo--;
+		}
 	}
 
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		Loader.unload("equipments/BulletPixel.png");
 	}
 
 	@Override
-	public void draw(Batch batch, float parentAlpha)
-	{
-		if(onMap)
-			batch.draw(sprite, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+	public void draw(Batch batch, float parentAlpha) {
+		if (onMap)
+			batch.draw(sprite, getX(), getY(), getOriginX(), getOriginY(),
+					getWidth(), getHeight(), getScaleX(), getScaleY(),
+					getRotation());
 	}
 
 	@Override
-	public void act(float delta)
-	{
+	public void act(float delta) {
 		// TODO Auto-generated method stub
 
 	}
